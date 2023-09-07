aa429b77689b40ea7a46837f9fce117a1a98df09
Siddhant H <s.hittalkeri@bm-systems.in>
2023-09-07 11:21:38 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [catFact, setCatFact] = useState("");
+  const [name, setName] = useState("");
+  const [ageData, setAgeData] = useState(null);
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then(
+      (res) => {
+        setCatFact(res.data.fact);
+      }
+    );
+  };
+
+  const fetchAgeData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then(
+      (res) => {
+        setAgeData(res.data);
+      }
+    );
+  };
+
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse);
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchCatFact}>Generate Cat Facts</button>
+      <p>{catFact}</p>
+
+      <hr></hr>
+      <br></br>
+
+      <input
+        type="text"
+        placeholder="Ex. John.."
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      ></input>
+      <button onClick={fetchAgeData}>Predict Age</button>
+      <h3>Name: {ageData?.name}</h3>
+      <h3>Predicted Age: {ageData?.age}</h3>
+      <h3>Count: {ageData?.count}</h3>
+
+      <hr></hr>
+      <br></br>
+
+      <h3>Generate An Excuse</h3>
+      <button onClick={() => fetchExcuse("party")}>Party</button>
+      <button onClick={() => fetchExcuse("family")}>Family</button>
+      <button onClick={() => fetchExcuse("office")}>Office</button>
+      <p>{generatedExcuse}</p>
     </div>
   );
 }
